cc073dde91137cada0173fe78ec7d5479890439b
zhanyl <turtemirova.zhanyl@gmail.com>
2021-01-11 14:21:00 +0600

    change app.js, add delete, add functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import "./App.scss";
+import React, { useState, useEffect } from "react";
 import TaskCreate from "./components/TaskCreate/TaskCreate";
 import Header from "./components/Header/Header";
 import TasksList from "./components/TasksList/TasksList";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 0, text: "test task", done: false },
+    { id: 1, text: "test task2", done: false },
+    { id: 2, text: "test task3", done: false },
+  ]);
+
+  const setTasksHandler = (item) => {
+    setTasks([...tasks, { id: tasks.length, text: item }]);
+  };
+
+  const deleteHandler = (id) => {
+    const newTasks = tasks;
+    var removeIndex = tasks
+      .map((item) => {
+        return item.id;
+      })
+      .indexOf(id);
+    if (removeIndex > 0) {
+      newTasks.splice(removeIndex, 1);
+    }
+    setTasks(newTasks);
+    console.log("????", tasks);
+  };
+
   return (
     <div className="Wrapper">
       <Header />
-      <TaskCreate />
-      <TasksList />
+      <TaskCreate setTasksProps={(item) => setTasksHandler(item)} />
+      <TasksList tasks={tasks} deleteItem={(id) => deleteHandler(id)} />
     </div>
   );
 }
